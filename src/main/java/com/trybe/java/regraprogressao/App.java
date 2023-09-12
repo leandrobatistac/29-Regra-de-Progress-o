d11ce4129839858319f30ceb908b510e96ad5754
Leandro <leandrobcrv@gmail.com>
2023-09-12 14:01:13 -0300

    Requisito 03

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -19,6 +19,10 @@ public class App {
     String[] nome = new String[qtdAtividades];
     Float[] peso = new Float[qtdAtividades];
     Float[] nota = new Float[qtdAtividades];
+    float notaTotal = 0;
+    float pesoTotal = 0;
+    float notaFinal = 0;
+
 
     for (int atv = 1; atv <= qtdAtividades; atv += 1) {
       System.out.println("Digite o nome da atividade " + atv + ":");
@@ -29,6 +33,21 @@ public class App {
 
       System.out.println("Digite a nota obtida para " + nome[atv - 1] + ":");
       nota[atv - 1] = Float.parseFloat(scanner.nextLine());
+
+      notaTotal = notaTotal + (peso[atv - 1] * nota[atv - 1]);
+      pesoTotal = pesoTotal + peso[atv - 1];
+    }
+
+    notaFinal = notaTotal / pesoTotal;
+
+    if (notaFinal >= 85) {
+      System.out.println("Parabéns! Você alcançou " + notaFinal
+          + "%! E temos o prazer de informar que você obteve aprovação! ");
+    } else {
+      System.out.println("Lamentamos informar que, "
+          + "com base na sua pontuação alcançada neste período, "
+          + notaFinal + "%, você não atingiu a pontuação "
+          + "mínima necessária para sua aprovação.");
     }
 
     scanner.close();
