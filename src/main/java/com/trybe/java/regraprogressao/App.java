8ae4279ae0f9c97d958664ae2bb0d1a5c939c68e
Leandro <leandrobcrv@gmail.com>
2023-09-12 13:23:26 -0300

    Correcoes Linter

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 
 public class App {
 
-    /**
-   * Metodo main.
-   */
+  /**
+ * Metodo main.
+ */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Digite a quantidade de atividades para cadastrar:");
