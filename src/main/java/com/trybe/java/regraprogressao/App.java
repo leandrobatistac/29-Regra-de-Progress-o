d476467a6b10af04dfc638b7aff7088899e323de
Leandro <leandrobcrv@gmail.com>
2023-09-12 13:32:54 -0300

    Requisito 02

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -15,16 +15,20 @@ public class App {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Digite a quantidade de atividades para cadastrar:");
     int qtdAtividades = Integer.parseInt((scanner.nextLine()));
-    
+
     String[] nome = new String[qtdAtividades];
     Float[] peso = new Float[qtdAtividades];
+    Float[] nota = new Float[qtdAtividades];
 
-    for (int i = 1; i <= qtdAtividades; i++) {
-      System.out.println("Digite o nome da atividade " + i + ":");
-      nome[i - 1] = scanner.nextLine();
+    for (int atv = 1; atv <= qtdAtividades; atv+=1) {
+      System.out.println("Digite o nome da atividade " + atv + ":");
+      nome[atv] = scanner.nextLine();
 
-      System.out.println("Digite o peso da atividade " + i + ":");
-      peso[i - 1] = Float.parseFloat(scanner.nextLine());
+      System.out.println("Digite o peso da atividade " + atv + ":");
+      peso[atv] = Float.parseFloat(scanner.nextLine());
+
+      System.out.println("Digite a nota obtida para " + nome[atv] + ":");
+      nota[atv] = Float.parseFloat(scanner.nextLine());
     }
 
     scanner.close();
